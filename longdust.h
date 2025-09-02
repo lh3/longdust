1d3f2eb3080927f5a507358d67af308f94722fba
Heng Li <lh3@me.com>
2025-09-02 11:17:49 -0400

    Release longdust-1.0 (r48)

--- longdust.h
+++ longdust.h
@@ -1,7 +1,7 @@
 #ifndef LONGDUST_H
 #define LONGDUST_H
 
-#define LD_VERSION "r25"
+#define LD_VERSION "1.0-r48"
 
 #include <stdint.h>
 
@@ -21,12 +21,44 @@ typedef struct ld_data_s ld_data_t;
 extern "C" {
 #endif
 
+/**
+ * Initialize default parameters
+ */
 void ld_opt_init(ld_opt_t *opt);
+
+/**
+ * Initialize the internal buffer for LCR identification
+ *
+ * @param km       NULL or memory space returned by km_init()
+ * @param opt      setting
+ *
+ * @return initialized buffer
+ */
 ld_data_t *ld_data_init(void *km, const ld_opt_t *opt);
+
+/**
+ * Deallocate the buffer
+ */
 void ld_data_destroy(ld_data_t *ld);
 
+/**
+ * Find LCRs on the forward strand
+ */
 void ld_dust1(ld_data_t *ld, int64_t len, const uint8_t *seq);
+
+/**
+ * The union of LCRs found on both strands
+ */
 void ld_dust2(ld_data_t *ld, int64_t len, const uint8_t *seq);
+
+/**
+ * Get the list of LCRs stored in the buffer
+ *
+ * @param ld        result buffer
+ * @param n         number of intervals (out)
+ *
+ * @return array of intervals of length _n_ (read-only)
+ */
 const ld_intv_t *ld_get_intv(const ld_data_t *ld, int64_t *n);
 
 #ifdef __cplusplus
